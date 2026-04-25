a162b78fa64cb10104bace8f9800c905cb99c2f1
suryanadh <darasurya81@gmail.com>
2026-04-25 23:38:38 +0530

    added changes

--- Backend/src/main/java/com/example/demo/controller/EmployeeController.java
+++ Backend/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,3 +1,5 @@
+package com.example.demo.controller;
+
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import com.example.demo.model.Employee;
--- Backend/src/main/java/com/example/demo/service/EmployeeService.java
+++ Backend/src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,3 +1,5 @@
+package com.example.demo.service;
+
 import org.springframework.stereotype.Service;
 import java.util.List;
 import com.example.demo.model.Employee;
